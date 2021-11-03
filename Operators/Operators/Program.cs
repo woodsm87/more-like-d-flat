41e007a6ff52fc5e6b963870d5960affbe781db3
IBadPennyI <woodsm87@gmail.com>
2021-11-03 15:11:39 -0500

    adds operator

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(a + b * c);
+            Console.WriteLine(a != b);
+            Console.WriteLine(!(a != b)); // that's bad
+            Console.WriteLine(c > b || c == a);
+
+            /*  
+             * Can use in-line multiplication and division ( *= , /= )
+             * 
+             * ! is used as not ( !a )
+             * 
+             * In C#, "postfix increment" results in increment after assignment/operation
+             *  int a = 1;
+             *  int b = a++;
+             * 
+             *  a = 2, b = 1
+             * 
+             * "prefix increment" performs increment prior to assignment/operation
+             *  int a = 1;
+             *  int b = ++a;
+             * 
+             *  a = 2, b = 2
+             */
+        }
+    }
+}
