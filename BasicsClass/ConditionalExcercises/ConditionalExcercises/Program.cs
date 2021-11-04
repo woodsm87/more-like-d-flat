17ce167928ddcd273ebfadccbec0f5901c8edf21
IBadPennyI <woodsm87@gmail.com>
2021-11-04 15:40:29 -0500

    conditional exercise 1

---
+++ BasicsClass/ConditionalExcercises/ConditionalExcercises/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConditionalExcercises
+{
+    public enum Exercise
+    {
+        Valid = 1,
+        Max = 2,
+        Image = 3,
+        Speed = 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select an exercise 1-4:");
+            var exercise = (Exercise)Convert.ToInt32(Console.ReadLine());
+
+            switch (exercise)
+            {
+                case Exercise.Valid:
+                    Console.WriteLine("You have chosen exercise 1: Validity test.\nPlease enter a number to test:");
+                    var input = Convert.ToInt32(Console.ReadLine());
+                    Validity(input);
+                    break;
+
+                default:
+                    Console.WriteLine("Bad input error.....");
+                    break;
+            }
+            
+
+
+        }
+        public static void Validity(int inputNum)
+        {
+            if (inputNum < 10 && inputNum > 0)
+            {
+                Console.WriteLine("Valid entry.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry.");
+            }
+        }
+    }
+}
